8a52408dd78581ebcb7df78d68d8018f5a1a53d7
Hanna Tariku <hannatariku1996@gmail.com>
2025-06-10 22:45:44 +0300

    update index.tsx

--- alx-project-0x03/pages/index.tsx
+++ alx-project-0x03/pages/index.tsx
@@ -1,7 +1,15 @@
 import Button from "@/components/common/Button";
 import { useRouter } from "next/router";
-
 import { PageRouteProps } from "@/interface";
+
+// const Home = () => {
+//   return (
+//    <div>
+//     <h1> Welcome to Splash App </h1>
+//    </div>
+// }
+
+// export default Home;
 
 export default function Home() {
   const router = useRouter();
