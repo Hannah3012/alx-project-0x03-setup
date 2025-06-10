bdd07ba40f84198f87f99ebcd630ee79fe260f53
Hanna Tariku <hannatariku1996@gmail.com>
2025-06-10 22:24:47 +0300

    moved interfaces to interface directory

--- alx-project-0x03/pages/index.tsx
+++ alx-project-0x03/pages/index.tsx
@@ -1,13 +1,13 @@
 import Button from "@/components/common/Button";
 import { useRouter } from "next/router";
 
-import { pageRouteProps } from "@/interface";
+import { PageRouteProps } from "@/interface";
 
 export default function Home() {
   const router = useRouter();
 
   // Imperative routing with useRouter
-  const routeToNextPage = ({ pageRoute }: pageRouteProps) => {
+  const routeToNextPage = ({ pageRoute }: PageRouteProps) => {
     router.push(pageRoute, undefined, { shallow: false });
   };
 
